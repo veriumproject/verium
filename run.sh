87c9580391193f1ecf625a4aa5f1bd0d1980b544
veriumproject <34689234+veriumproject@users.noreply.github.com>
2018-05-22 17:12:13 +0200

    Update run.sh
    Fix aws worker

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-/home/ubuntu/verium-master/cpuminer-opt-3.6.7/cpuminer -a scrypt:1048576 -o stratum+tcp://eu.poolinat0r.com:4447 -u veriumproject.aws -p 'm*5&X%K&mH7nX6vm'
+/home/ubuntu/verium-master/cpuminer-opt-3.6.7/cpuminer -a scrypt:1048576 -o stratum+tcp://eu.poolinat0r.com:4447 -u veriumproject.vrm-p 'm*5&X%K&mH7nX6vm'
